704a5935f5f5b9e8a98e58b8b1db666b41815a31
chenith <chenith@oneit.com.au>
2017-03-18 10:37:30 +0530

    Initial commit with plugins.

---
+++ installPlugins.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+ionic plugin add cordova-plugin-app-version
+ionic plugin add cordova-plugin-console
+ionic plugin add cordova-plugin-device
+ionic plugin add cordova-plugin-splashscreen
+ionic plugin add cordova-plugin-statusbar
+ionic plugin add cordova-plugin-whitelist
+ionic plugin add cordova-sqlite-storage
+ionic plugin add ionic-plugin-keyboard
+ionic plugin add cordova-plugin-network-information
+cordova plugin add cordova-plugin-network-information
+ionic plugin add cordova-plugin-http
+ionic plugin add cordova-sms-plugin
